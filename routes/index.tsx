bdd87a6f8db5eb81c5212573718dc6c25e8d2107
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-10-03 11:49:26 +0900

    feat: ポケモンのリストを表示するメインのコンポーネントを作成した

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,29 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import PokemonItem from "../islands/PokemonItem.tsx";
 
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <div class="flex flex-col items-center">
+      <header class="my-8">
+        <h1 class="text-3xl font-semibold">ポケモン図鑑</h1>
+      </header>
+      <div class="flex flex-wrap justify-center gap-2">
+        {/* <!-- PokemonItem コンポーネントを繰り返し表示 --> */}
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        <PokemonItem />
+        {/* <!-- 必要なだけ繰り返す --> */}
       </div>
+      <button class="my-8 py-2 px-4 bg-blue-500 text-white rounded-lg">
+        もっとみる
+      </button>
     </div>
   );
 }
